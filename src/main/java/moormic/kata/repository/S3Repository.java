423e9264af4652ef563b8f2116fae9096dda2d3f
mmoor <mmoore5411@gmail.com>
2025-01-20 15:10:41 -0500

    Initial S3 test

--- src/main/java/moormic/kata/repository/S3Repository.java
+++ src/main/java/moormic/kata/repository/S3Repository.java
@@ -7,10 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.Delete;
-import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.File;
 import java.util.HashMap;
@@ -48,7 +45,8 @@ public class S3Repository {
     }
 
     public List<S3Object> list(String bucketName) {
-        return s3Client.listObjectsV2(request -> request.bucket(bucketName).build()).contents();
+        var listIterable = s3Client.listObjectsV2Paginator(request -> request.bucket(bucketName).maxKeys(1000).build());
+        return listIterable.stream().map(ListObjectsV2Response::contents).flatMap(List::stream).toList();
     }
 
     public void delete(String bucketName, List<String> keys) {
